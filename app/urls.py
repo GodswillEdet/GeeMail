8cc4fc3ade1fc57bd26a6bf6497127ade888cf44
GodswillEdet <gemzeeedet@gmail.com>
2023-01-17 19:02:59 +0100

    Added new url

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('', views.send_email, name='home'),
+    path('sent/', views.sent_email, name='sent')
 ]
