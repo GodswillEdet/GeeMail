b4fed2f0f71ba318591d8f7ed3274b85f9d66cf9
GodswillEdet <gemzeeedet@gmail.com>
2023-01-24 18:18:30 +0100

    Modified to accept users input

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+import imp
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 def send_email(request):
+    if request.method == "POST":
+        subject = request.POST.get('subject')
+        message = request.POST.get('message') 
+        email_from = request.POST.get('email')
+        recipient_list = request.POST.get('email')
+        
+        print(subject, message, email_from, recipient_list)
+        return redirect('sent')
     return render(request, 'index.html')
 
 def sent_email(request):
