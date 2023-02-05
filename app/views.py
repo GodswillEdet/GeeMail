f474e30fb41dd8d7de730d735c8af6b66c638686
GodswillEdet <gemzeeedet@gmail.com>
2023-02-05 17:49:26 +0100

    improve theemail sending logic

--- app/views.py
+++ app/views.py
@@ -1,17 +1,25 @@
 import imp
 from django.shortcuts import render, redirect
+from django.conf import settings
 from django.core.mail import send_mail
+
+
+def home(request):
+    return render(request, 'index.html')
 
 def send_email(request):
     if request.method == "POST":
         subject = request.POST.get('subject')
         message = request.POST.get('message') 
-        email_from = request.POST.get('email')
+        email_from = settings.EMAIL_HOST_USER
         recipient_list = request.POST.get('email')
         
         print(subject, message, email_from, recipient_list)
-        return redirect('sent')
-    return render(request, 'index.html')
-
-def sent_email(request):
+        send_mail( subject, message, email_from, recipient_list )
+    return redirect('sent')
+    
+def sent(request):
     return render(request, 'success.html')
+
+
+
